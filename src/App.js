9693ae931dbd8b579cadba586ed6715c8221c29b
Mitchel Cabuloy <mixxorz@gmail.com>
2018-02-02 17:15:48 +0800

    Add number to words
    * Finalize template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
+import numberToWords from 'number-to-words'
+
 import './App.scss'
+
 
 const TextField = ({label, onChange, id}) => (
   <div className="text-field">
@@ -20,19 +23,36 @@ class App extends Component {
   }
 
   handleNameChange(event) {
-    this.setState({name: event.target.value})
+    this.setState({name: event.target.value.trim()})
   }
 
   handleDateChange(event) {
-    this.setState({date: event.target.value})
+    this.setState({date: event.target.value.trim()})
   }
 
   handleAmountChange(event) {
-    this.setState({amount: event.target.value})
+    this.setState({amount: parseFloat(event.target.value, 10)})
   }
 
   render() {
     const {name, date, amount} = this.state
+
+    const amountDisplay = amount.toLocaleString(
+      ['en-ph'], {
+      style: 'decimal',
+      minimumFractionDigits: 2,
+    });
+
+    let amountWords = ''
+
+    if (amount) {
+      amountWords = numberToWords.toWords(amount);
+
+      if (amount % 1) {
+        const decimal = (amount % 1 * 100).toFixed(0);
+        amountWords = `${amountWords} and ${decimal}/100`
+      }
+    }
 
     return (
       <div className="app-block">
@@ -45,8 +65,8 @@ class App extends Component {
         <div className="check-paper">
           <div className="check-paper__name">{name.toUpperCase()}</div>
           <div className="check-paper__date">{date.toUpperCase()}</div>
-          <div className="check-paper__amount">{amount}</div>
-          <div className="check-paper__words">{amount}</div>
+          <div className="check-paper__amount">**{amountDisplay}**</div>
+          <div className="check-paper__words">**{amountWords.toUpperCase()}**</div>
         </div>
       </div>
     )
