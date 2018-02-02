293a51ad1b16124a9647d30eb6cd5f74a398492f
Mitchel Cabuloy <mixxorz@gmail.com>
2018-02-02 16:37:20 +0800

    Add textbox styles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React, { Component } from 'react'
+import './App.scss'
+
+const TextField = ({label, onChange, id}) => (
+  <div className="text-field">
+    <label className="text-field__label" htmlFor={id}>{label}</label>
+    <input className="text-field__input" name={id} type="text" onChange={onChange} />
+  </div>
+)
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      name: '',
+      date: '',
+      amount: '',
+    }
+  }
+
+  handleNameChange(event) {
+    this.setState({name: event.target.value})
+  }
+
+  handleDateChange(event) {
+    this.setState({date: event.target.value})
+  }
+
+  handleAmountChange(event) {
+    this.setState({amount: event.target.value})
+  }
+
   render() {
+    const {name, date, amount} = this.state
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app-block">
+        <div className="fields-block">
+          <TextField label="Name" id="name" onChange={(ev) => this.handleNameChange(ev)} />
+          <TextField label="Date" id="date" onChange={(ev) => this.handleDateChange(ev)} />
+          <TextField label="Amount" id="amount" onChange={(ev) => this.handleAmountChange(ev)} />
+        </div>
+
+        <div className="check-paper">
+          <div className="check-paper__name">{name.toUpperCase()}</div>
+          <div className="check-paper__date">{date.toUpperCase()}</div>
+          <div className="check-paper__amount">{amount}</div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
