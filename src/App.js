adc428813c1388893951964c8dd56c519df98b23
Mitchel Cabuloy <mixxorz@gmail.com>
2018-02-09 09:58:02 +0800

    Small improvements
    * Add usage instructions
    * Add moment.js for date parsing and display
    * Add support for amount with commas
    * Add smart title updating

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
+import moment from 'moment'
 import numberToWords from 'number-to-words'
 
 import './App.scss'
 
+const DATE_FORMAT = 'MMM D, YYYY'
 
-const TextField = ({label, onChange, id}) => (
+
+const TextField = ({label, onChange, id, value}) => (
   <div className="text-field">
     <label className="text-field__label" htmlFor={id}>{label}</label>
-    <input className="text-field__input" name={id} type="text" onChange={onChange} />
+    <input className="text-field__input" name={id} type="text" onChange={onChange} value={value} />
   </div>
 )
 
@@ -17,9 +20,14 @@ class App extends Component {
 
     this.state = {
       name: '',
-      date: '',
+      date: moment(new Date()).format(DATE_FORMAT),
       amount: '',
     }
+  }
+
+  componentDidUpdate() {
+    const {name, date} = this.state
+    document.title = `${moment(date).format('YYYY-MM-DD')}-${name.toLowerCase().replace(/ /g, '-')}`
   }
 
   handleNameChange(event) {
@@ -31,11 +39,18 @@ class App extends Component {
   }
 
   handleAmountChange(event) {
-    this.setState({amount: parseFloat(event.target.value, 10)})
+    const value = event.target.value.trim().replace(/,/g, '')
+
+    if (value) {
+      this.setState({amount: parseFloat(value, 10)})
+    } else {
+      this.setState({amount: ''})
+    }
   }
 
   render() {
     const {name, date, amount} = this.state
+    const dateMoment = moment(date)
 
     const amountDisplay = amount.toLocaleString(
       ['en-ph'], {
@@ -63,13 +78,13 @@ class App extends Component {
         </div>
         <div className="fields-block">
           <TextField label="Name" id="name" onChange={(ev) => this.handleNameChange(ev)} />
-          <TextField label="Date" id="date" onChange={(ev) => this.handleDateChange(ev)} />
+          <TextField label="Date" id="date" onChange={(ev) => this.handleDateChange(ev)} value={date} />
           <TextField label="Amount" id="amount" onChange={(ev) => this.handleAmountChange(ev)} />
         </div>
 
         <div className="check-paper">
           <div className="check-paper__name">{name.toUpperCase()}</div>
-          <div className="check-paper__date">{date.toUpperCase()}</div>
+          <div className="check-paper__date">{dateMoment.format(DATE_FORMAT).toUpperCase()}</div>
           <div className="check-paper__amount">**{amountDisplay}**</div>
           <div className="check-paper__words">**{amountWords.toUpperCase()}**</div>
         </div>
